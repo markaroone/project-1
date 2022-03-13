c99f0d1d222d5506930e044af0fbe91e7e4562ca
Christian <cfajardo25@gmail.com>
2022-03-13 18:19:00 +0800

    fix: fix scrolling animation not opening external html files

--- Project-1/js/script.js
+++ Project-1/js/script.js
@@ -20,22 +20,26 @@ for (const [key, _] of faqAccordionEl.entries()) {
 }
 
 // Smooth Scrolling Animation
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
+{
+  /*
+  allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
 
-    const href = link.getAttribute("href");
-    console.log(href);
+      const href = link.getAttribute("href");
+      console.log(href);
 
-    // Scroll back to top
-    if (href === "#") {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    } else if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
+      // Scroll back to top
+      if (href === "#") {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      } else if (href !== "#" && href.startsWith("#")) {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({ behavior: "smooth" });
+      }
+    });
   });
-});
+  */
+}
