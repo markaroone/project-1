3d8174ba9b8a5bea14ac92e52c6e31a1890a8bc8
Christian <cfajardo25@gmail.com>
2022-03-13 17:41:57 +0800

    feat: add scrolling animation

--- Project-1/js/script.js
+++ Project-1/js/script.js
@@ -1,18 +1,41 @@
 "use strict";
 
-const faqAccordionEl = document.querySelectorAll(".faq-accordion");
 const toggleButtonEl = document.querySelector(".toggle-button");
 const navBarLinksEl = document.querySelector(".navbar-links");
+const faqAccordionEl = document.querySelectorAll(".faq-accordion");
 const faqIconEl = document.querySelector("faq-icon");
+const allLinks = document.querySelectorAll("a:link");
 
+// Mobile Navigation
 toggleButtonEl.addEventListener("click", () => {
   navBarLinksEl.classList.toggle("active");
 });
 
-for (const [key, val] of faqAccordionEl.entries()) {
+// FAQ Section Accordion
+for (const [key, _] of faqAccordionEl.entries()) {
   faqAccordionEl[key].addEventListener("click", () => {
     faqAccordionEl[key].classList.toggle("open");
-
     faqAccordionEl[key].classList.toggle("invert-icon");
   });
 }
+
+// Smooth Scrolling Animation
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
