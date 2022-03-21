0a398eb973dd8f108cd6c5640f8b99234259db4f
Christian <cfajardo25@gmail.com>
2022-03-21 19:10:13 +0800

    fix: major revision - applied mobile first approach and fix problems from reviews

--- Project-1/js/script.js
+++ Project-1/js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // MOBILE NAVIGATION
 {
@@ -9,21 +9,21 @@
   };
 
   //   DOM Elements
-  const htmlEl = document.querySelector("html");
-  const hamburgerBtnEl = document.querySelector(".hamburger");
-  const mobileNavEl = document.querySelector(".mobile-nav");
-  const mobileNavLinksEl = document.querySelectorAll(".mobile-nav-link");
+  const htmlEl = document.querySelector('html');
+  const hamburgerBtnEl = document.querySelector('.hamburger');
+  const mobileNavEl = document.querySelector('.mobile-nav');
+  const mobileNavLinksEl = document.querySelectorAll('.mobile-nav-link');
 
   //   Local var
-  const active = "active";
+  const active = 'active';
 
   //   Events
-  hamburgerBtnEl.addEventListener("click", () => {
+  hamburgerBtnEl.addEventListener('click', () => {
     toggleMobileNav();
   });
 
   mobileNavLinksEl.forEach((link) => {
-    link.addEventListener("click", () => {
+    link.addEventListener('click', () => {
       toggleMobileNav();
     });
   });
@@ -31,23 +31,23 @@
 
 // FAQ Section Accordion
 {
-  const faqAccordionEls = document.querySelectorAll(".faq-accordion");
+  const faqAccordionEls = document.querySelectorAll('.faq-accordion');
 
   faqAccordionEls.forEach((accordion) => {
-    accordion.addEventListener("click", () => {
-      const currentAccordion = document.querySelector(".faq-accordion.open");
+    accordion.addEventListener('click', () => {
+      const currentAccordion = document.querySelector('.faq-accordion.open');
 
       if (currentAccordion && currentAccordion !== accordion) {
-        currentAccordion.classList.toggle("open");
-        currentAccordion.querySelector(".faq-hidden").style.maxHeight = 0;
+        currentAccordion.classList.toggle('open');
+        currentAccordion.querySelector('.faq-hidden').style.maxHeight = 0;
       }
 
-      accordion.classList.toggle("open");
+      accordion.classList.toggle('open');
 
-      const accordionBody = accordion.querySelector(".faq-hidden");
+      const accordionBody = accordion.querySelector('.faq-hidden');
 
-      if (accordion.classList.contains("open")) {
-        accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
+      if (accordion.classList.contains('open')) {
+        accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
       } else {
         accordionBody.style.maxHeight = 0;
       }
@@ -55,27 +55,4 @@
   });
 }
 
-// Smooth Scrolling Animation
-{
-  /*
-  allLinks.forEach(function (link) {
-    link.addEventListener("click", function (e) {
-      e.preventDefault();
-
-      const href = link.getAttribute("href");
-      console.log(href);
-
-      // Scroll back to top
-      if (href === "#") {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-        });
-      } else if (href !== "#" && href.startsWith("#")) {
-        const sectionEl = document.querySelector(href);
-        sectionEl.scrollIntoView({ behavior: "smooth" });
-      }
-    });
-  });
-  */
-}
+// console.log(typeof typeof (1 + 1));
