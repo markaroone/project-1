07e19a6098f0786ab22aa5994dc4a32da43f8aa4
Christian <cfajardo25@gmail.com>
2022-07-02 12:54:11 +0800

    fix: forgot to add fix haha

--- Project-6/frontend/src/components/activity-item/ActivityNotes.js
+++ Project-6/frontend/src/components/activity-item/ActivityNotes.js
@@ -35,7 +35,9 @@ const ActivityNotes = ({ data }) => {
   };
 
   const onHideTextArea = () => {
-    setTimeout(setIsEditing(false), 100);
+    setTimeout(() => {
+      setIsEditing(false);
+    }, 100);
   };
 
   return (
